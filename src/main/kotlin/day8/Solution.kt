c58ad56e7d4758fcb7c5ca49d92a999629d0aeaa
crab-apple <andreu.bastardas@gmail.com>
2023-12-08 08:14:53 +0100

    Day 8 part 2 brute force, not finishing on real input

--- src/main/kotlin/day8/Solution.kt
+++ src/main/kotlin/day8/Solution.kt
@@ -11,28 +11,12 @@ fun main() {
 
 fun solvePart1(input: List<String>): Int {
 
-    val directions = input[0]
-
-    val nodes: Map<String, Pair<String, String>> = input.drop(2)
-        .associateBy(
-            { it.split("=")[0].trim() },
-            {
-                it.split("=")[1].trim()
-                    .removeSurrounding("(", ")")
-                    .split(", ")
-                    .let { it[0] to it[1] }
-            }
-        )
+    val map = DesertMap.parse(input)
 
     var currentNode = "AAA"
     var count = 0
     do {
-        val direction = directions[count % directions.length]
-        currentNode = when (direction) {
-            'L' -> nodes[currentNode]!!.first
-            'R' -> nodes[currentNode]!!.second
-            else -> throw IllegalStateException()
-        }
+        currentNode = map.nextNode(currentNode, count)
         count++
     } while (currentNode != "ZZZ")
 
@@ -40,5 +24,48 @@ fun solvePart1(input: List<String>): Int {
 }
 
 fun solvePart2(input: List<String>): Int {
-    return input.size
+
+    val map = DesertMap.parse(input)
+
+    var currentNodes = map.nodes.keys.filter { it.endsWith('A') }
+    var count = 0
+    do {
+        currentNodes = currentNodes.map { map.nextNode(it, count) }
+        count++
+    } while (!currentNodes.all { it.endsWith('Z') })
+
+    return count
+}
+
+data class DesertMap(val directions: String, val nodes: Map<String, Pair<String, String>>) {
+
+    fun nextNode(currentNode: String, step: Int): String {
+        val direction = directions[step % directions.length]
+        return when (direction) {
+            'L' -> nodes[currentNode]!!.first
+            'R' -> nodes[currentNode]!!.second
+            else -> throw IllegalStateException()
+        }
+    }
+
+    companion object {
+
+        fun parse(input: List<String>): DesertMap {
+
+            val directions = input[0]
+
+            val nodes: Map<String, Pair<String, String>> = input.drop(2)
+                .associateBy(
+                    { it.split("=")[0].trim() },
+                    {
+                        it.split("=")[1].trim()
+                            .removeSurrounding("(", ")")
+                            .split(", ")
+                            .let { it[0] to it[1] }
+                    }
+                )
+
+            return DesertMap(directions, nodes)
+        }
+    }
 }
