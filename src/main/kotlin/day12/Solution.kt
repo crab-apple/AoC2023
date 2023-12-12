9eea0f5abeaecc9ff4e218176d37ee0426408486
crab-apple <andreu.bastardas@gmail.com>
2023-12-12 22:28:32 +0100

    Day 12 part 2

--- src/main/kotlin/day12/Solution.kt
+++ src/main/kotlin/day12/Solution.kt
@@ -13,12 +13,35 @@ fun solvePart1(input: List<String>): Long {
     return input.sumOf { line ->
         val str = line.split(" ").first()
         val groups = line.split(" ").last().split(",").map { it.toInt() }
-        countArrangements(str, groups).toLong()
+        countArrangements(str, groups)
     }
 }
 
 fun solvePart2(input: List<String>): Long {
-    return input.size.toLong()
+    return input.sumOf { line ->
+        val str = repeatFiveTimes(line.split(" ").first(), '?')
+        val groups = repeatFiveTimes(line.split(" ").last(), ',').split(",").map { it.toInt() }
+        countArrangements(str, groups)
+    }
+}
+
+private fun repeatFiveTimes(input: String, delimiter: Char): String {
+    return input.plus(delimiter).repeat(5).dropLast(1)
+}
+
+fun repeatString(input: String, times: Int): String {
+    return repeat(input.toList(), times, '?').joinToString("")
+}
+
+fun <T> repeat(input: List<T>, times: Int, delimiter: T? = null): List<T> {
+    val result = mutableListOf<T>()
+    for (i in 1..times) {
+        result.addAll(input)
+        if (i < times && delimiter != null) {
+            result.add(delimiter)
+        }
+    }
+    return result
 }
 
 fun findPossibleStrings(s: String): List<String> {
@@ -36,12 +59,89 @@ fun findPossibleStrings(s: String, accumulator: MutableList<String>) {
     findPossibleStrings(s.replaceFirst('?', '#'), accumulator)
 }
 
-fun countArrangements(input: String, groups: List<Int>): Int {
-    return findPossibleStrings(input).count { groupsOf(it) == groups }
+fun countArrangements(input: String, groups: List<Int>): Long {
+    return countArrangementsMemoized(input, groups, mutableMapOf())
 }
 
-fun groupsOf(input: String): List<Int> {
-    return input.split(".")
-        .filter { it.isNotBlank() }
-        .map { it.length }
+fun countArrangementsMemoized(
+    input: String,
+    groups: List<Int>,
+    cache: MutableMap<Pair<String, List<Int>>, Long>
+): Long {
+    val key = Pair(input, groups)
+    if (cache.containsKey(key)) {
+        return cache[key]!!
+    }
+    val result = countArrangements(input, groups, cache)
+    cache[key] = result
+    return result
 }
+
+fun countArrangements(input: String, groups: List<Int>, cache: MutableMap<Pair<String, List<Int>>, Long>): Long {
+
+    if (input.count { it == '#' } > groups.sumOf { it }) {
+        return 0
+    }
+
+    if (input.count { it == '#' || it == '?' } < groups.sumOf { it }) {
+        return 0
+    }
+
+    if (groups.isNotEmpty() && groups[0] == 0) {
+        return countArrangementsMemoized(input, groups.drop(1), cache)
+    }
+
+    if (input.startsWith(".")) {
+        return countArrangementsMemoized(input.drop(1), groups, cache)
+    }
+
+    if (input.startsWith("#")) {
+
+        if (groups.isEmpty()) {
+            return 0
+        }
+
+        if (groups[0] > input.length) {
+            return 0
+        }
+
+        if (input.take(groups[0]).contains(".")) {
+            return 0
+        }
+
+        if (input.length > groups[0] && input[groups[0]] == '#') {
+            return 0
+        }
+
+        if (input.take(groups[0] + 1).contains("?")) {
+            return countArrangementsMemoized(
+                input.mapIndexed { index, char ->
+                    if (index < groups[0]) '#'
+                    else if (index == groups[0]) '.'
+                    else char
+                }
+                    .joinToString(""),
+                groups,
+                cache
+            )
+        }
+
+        return countArrangementsMemoized(
+            input.drop(1),
+            groups.mapIndexed { index, group -> if (index == 0) group - 1 else group },
+            cache
+        )
+    }
+
+    if (input.contains("?")) {
+        return countArrangementsMemoized(input.replaceFirst('?', '.'), groups, cache) +
+                countArrangementsMemoized(input.replaceFirst('?', '#'), groups, cache)
+    }
+
+    if (groups.isNotEmpty()) {
+        return 0
+    }
+
+    return 1
+}
+
--- src/test/kotlin/day12/SolutionTest.kt
+++ src/test/kotlin/day12/SolutionTest.kt
@@ -4,6 +4,8 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.containsInAnyOrder
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class SolutionTest {
 
@@ -34,13 +36,59 @@ class SolutionTest {
         )
     }
 
+    @ParameterizedTest
+    @MethodSource("countArrangementsTestCases")
+    fun `count arrangements`(testCase: TestCase) {
+        assertThat(
+            countArrangements(testCase.input, testCase.groups),
+            `is`(testCase.expectedOutput)
+        )
+    }
+
     @Test
-    fun `count arrangements`() {
+    fun `count arrangements large input`() {
         assertThat(
             countArrangements(
-                ".??..??...?##.", listOf(1, 1, 3)
+                ".??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.",
+                listOf(1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3)
             ),
-            `is`(4)
+            `is`(16384)
+        )
+
+        assertThat(
+            countArrangements(
+                "???#??????",
+                listOf(1, 1, 1)
+            ),
+            `is`(16)
+        )
+        assertThat(
+            countArrangements(
+                repeatString("???#??????", 2),
+                repeat(listOf(1, 1, 1), 2)
+            ),
+            `is`(587)
+        )
+        assertThat(
+            countArrangements(
+                repeatString("???#??????", 3),
+                repeat(listOf(1, 1, 1), 3)
+            ),
+            `is`(24486)
+        )
+        assertThat(
+            countArrangements(
+                repeatString("???#??????", 4),
+                repeat(listOf(1, 1, 1), 4)
+            ),
+            `is`(1080018)
+        )
+        assertThat(
+            countArrangements(
+                repeatString("???#??????", 5),
+                repeat(listOf(1, 1, 1), 4)
+            ),
+            `is`(5173570)
         )
     }
 
@@ -59,5 +107,71 @@ class SolutionTest {
             ),
             `is`(21)
         )
+    }
+
+    @Test
+    fun testRepeat() {
+        assertThat(
+            repeat(listOf(1, 2, 3), 3),
+            `is`(listOf(1, 2, 3, 1, 2, 3, 1, 2, 3))
+        )
+    }
+
+    @Test
+    fun testRepeatString() {
+        assertThat(
+            repeatString("##", 3),
+            `is`("##?##?##")
+        )
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun countArrangementsTestCases() = listOf(
+            TestCase("Empty", "", listOf(), 1),
+            TestCase("Group size 1, matching", "#", listOf(1), 1),
+            TestCase("Group size 1, not matching", "#", listOf(), 0),
+            TestCase("Group size 2, matching", "##", listOf(2), 1),
+            TestCase("Group size 2, too big", "##", listOf(1), 0),
+            TestCase("Group size 2, too small", "##", listOf(3), 0),
+            TestCase("Leading dots, matching", "..##", listOf(2), 1),
+            TestCase("Leading dots, too big", "..##", listOf(1), 0),
+            TestCase("Leading dots, too small", "..##", listOf(3), 0),
+            TestCase("Trailing dots, matching", "##..", listOf(2), 1),
+            TestCase("Trailing dots, too big", "##..", listOf(1), 0),
+            TestCase("Trailing dots, too small", "##..", listOf(3), 0),
+            TestCase("Two groups, matching", "..##..##..", listOf(2, 2), 1),
+            TestCase("Two groups, first too big", "..###..##..", listOf(2, 2), 0),
+            TestCase("Two groups, second too big", "..##..###..", listOf(2, 2), 0),
+            TestCase("Two groups, first too small", "..#..##..", listOf(2, 2), 0),
+            TestCase("Two groups, second too small", "..##..#..", listOf(2, 2), 0),
+            TestCase("With question marks", "?..##..##..", listOf(2, 2), 1),
+            TestCase(
+                "Hash followed by many question marks, single group",
+                "#???????????????????????????",
+                listOf(10),
+                1
+            ),
+            TestCase("?#", listOf(1, 1), 0),
+            TestCase("??#", listOf(1, 1), 1),
+            TestCase("??", listOf(1, 1), 0),
+            TestCase("???", listOf(1, 1), 1),
+            TestCase("???.###", listOf(1, 1, 3), 1),
+            TestCase(".??..??...?##.", listOf(1, 1, 3), 4),
+            TestCase("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6), 1),
+            TestCase("????.#...#...", listOf(4, 1, 1), 1),
+            TestCase("????.######..#####.", listOf(1, 6, 5), 4),
+            TestCase("?###????????", listOf(3, 2, 1), 10),
+        )
+    }
+
+    data class TestCase(val name: String, val input: String, val groups: List<Int>, val expectedOutput: Long) {
+
+        constructor(input: String, groups: List<Int>, expectedOutput: Long) : this(input, input, groups, expectedOutput)
+
+        override fun toString(): String {
+            return name
+        }
     }
 }
