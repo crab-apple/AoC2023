cc5ea4cb74f9521abb3763ffb00813b7c2ff6903
crab-apple <andreu.bastardas@gmail.com>
2023-12-15 06:58:22 +0100

    Refactor day 15, perhaps overengineered a bit

--- src/main/kotlin/day15/Solution.kt
+++ src/main/kotlin/day15/Solution.kt
@@ -14,23 +14,62 @@ fun solvePart1(input: String): Int {
 }
 
 fun solvePart2(input: String): Int {
-    val boxes = (0..<256).map { LinkedHashMap<String, Int>() }
-    for (operation in input.split(",")) {
-        if (operation.contains('=')) {
-            val label = operation.substringBefore('=')
-            val focalLength = operation.substringAfter('=').toInt()
-            val boxNumber = hash(label)
-            boxes[boxNumber][label] = focalLength
-        } else {
-            val label = operation.substringBefore('-')
-            val boxNumber = hash(label)
-            boxes[boxNumber].remove(label)
-        }
+    val boxes = (0..<256).map { Box() }
+
+    for (op in input.split(",").map { parseOperation(it) }) {
+        op.apply(boxes[op.boxNumber()])
     }
 
     return boxes.mapIndexed { index, box ->
-        (index + 1) * box.entries.mapIndexed { position, entry -> (position+1) * entry.value }.sum()
+        (index + 1) * box.focusingPower()
     }.sum()
+}
+
+abstract class Operation(private val label: String) {
+
+    fun boxNumber() = hash(label)
+    abstract fun apply(box: Box)
+}
+
+class LensAddition(private val label: String, private val focalLength: Int) : Operation(label) {
+
+    override fun apply(box: Box) {
+        box.addLens(label, focalLength)
+    }
+}
+
+class LensRemoval(private val label: String) : Operation(label) {
+
+    override fun apply(box: Box) {
+        box.removeLens(label)
+    }
+}
+
+fun parseOperation(s: String): Operation {
+    val groups = Regex("([a-z]+)(.)(\\d*)").matchEntire(s)!!.groups
+    val label = groups[1]?.value ?: throw IllegalArgumentException()
+    val operationType = groups[2]?.value?.get(0) ?: throw IllegalArgumentException()
+    val operand = groups[3]?.value?.ifBlank { null }?.toInt()
+    return when (operationType) {
+        '=' -> LensAddition(label, operand!!)
+        '-' -> LensRemoval(label)
+        else -> throw IllegalArgumentException()
+    }
+}
+
+class Box {
+
+    private val lenses = LinkedHashMap<String, Int>()
+
+    fun addLens(label: String, focalLength: Int) {
+        lenses[label] = focalLength
+    }
+
+    fun removeLens(label: String) {
+        lenses.remove(label)
+    }
+
+    fun focusingPower(): Int = lenses.entries.mapIndexed { position, entry -> (position + 1) * entry.value }.sum()
 }
 
 fun hash(input: String): Int {
