75621f63ad0b2ccc2dbbd1ecc68ccac1b57b85d1
crab-apple <andreu.bastardas@gmail.com>
2023-12-19 16:12:46 +0100

    Represent predicates internally as a list of ranges

--- src/main/kotlin/day19/PartPredicate.kt
+++ src/main/kotlin/day19/PartPredicate.kt
@@ -2,22 +2,32 @@ package day19
 
 import java.util.function.Predicate
 
-data class PartPredicate(val category: Category, val threshold: Int, val operation: Char) : Predicate<Part> {
+class PartPredicate(private val ranges: Map<Category, IntRange>) : Predicate<Part> {
 
     override fun test(part: Part): Boolean {
-        return if (operation == '<')
-            part[category] < threshold
-        else
-            part[category] > threshold
+        return ranges.all { it.value.contains(part[it.key]) }
     }
 
     override fun toString(): String {
-        return "$category$operation$threshold"
+        return ranges.map { "${it.key}${it.value}" }.joinToString()
     }
 
     companion object {
 
-        fun of(category: Category, threshold: Int, operation: Char) = PartPredicate(category, threshold, operation)
+        fun of(category: Category, threshold: Int, operation: Char): PartPredicate {
+
+            return PartPredicate(Category.entries.associateWith { cat ->
+                if (cat == category) {
+                    if (operation == '<') {
+                        IntRange(0, threshold - 1)
+                    } else {
+                        IntRange(threshold + 1, 4000)
+                    }
+                } else {
+                    IntRange(0, 4000)
+                }
+            })
+        }
 
         fun parse(input: String): PartPredicate {
             val category = input.split("[<>]".toRegex())[0].let { Category.parse(it) }
