276f8893fcc11f9b5c62f882e084252ef151af71
crab-apple <andreu.bastardas@gmail.com>
2023-12-21 13:54:35 +0100

    Improve performance of day 21 part 1

--- src/main/kotlin/day21/Solution.kt
+++ src/main/kotlin/day21/Solution.kt
@@ -3,6 +3,7 @@ package day21
 import utils.Position
 import utils.println
 import utils.readInputOneString
+import java.util.*
 
 fun main() {
     val input = readInputOneString("day21/input")
@@ -17,32 +18,36 @@ fun solvePart1(input: String): Long {
 fun solvePart1(input: String, targetSteps: Int): Long {
 
     val lines = input.lines().map { it.toList() }
-    val allPositions = lines.indices.flatMap { row -> lines[0].indices.map { Position(row, it) } }
+    val allPositions = lines.indices.flatMap { row -> lines[0].indices.map { Position(row, it) } }.toSet()
 
     fun valAt(pos: Position) = lines[pos.row.toInt()][pos.col.toInt()]
 
-    val rocks = allPositions.filter { valAt(it) == '#' }
+    val rocks = allPositions.filter { valAt(it) == '#' }.toSet()
 
     val startingPosition = allPositions.single { valAt(it) == 'S' }
-    val reachabilities = allPositions.associateWith { mutableSetOf<Int>() }
 
-    val pendingToEvaluate = mutableSetOf<Pair<Position, Int>>()
-    pendingToEvaluate.add(Pair(startingPosition, 0))
+    val reachabilities = mutableMapOf<Position, Int>()
+    val pendingToVisit = LinkedList<Pair<Position, Int>>()
 
-    while (pendingToEvaluate.isNotEmpty()) {
-        val current = pendingToEvaluate.first()
-        pendingToEvaluate.remove(current)
+    pendingToVisit.add(Pair(startingPosition, 0))
+
+    while (pendingToVisit.isNotEmpty()) {
+        val current = pendingToVisit.remove()
         val pos = current.first
         val steps = current.second
-        if (steps <= targetSteps && reachabilities[pos]!!.add(steps)) {
-            pos.allNeighbours()
-                .filter { allPositions.contains(it) }
-                .filter { !rocks.contains(it) }
-                .forEach { pendingToEvaluate.add(Pair(it, steps + 1)) }
+        if (reachabilities.putIfAbsent(pos, steps) != null) {
+            continue
         }
+        pos.allNeighbours()
+            .filter { allPositions.contains(it) }
+            .filter { !rocks.contains(it) }
+            .forEach {
+                pendingToVisit.add(Pair(it, steps + 1))
+            }
     }
 
-    return reachabilities.filter { it.value.contains(targetSteps) }.count().toLong()
+
+    return reachabilities.filter { it.value <= targetSteps && it.value % 2 == targetSteps % 2 }.count().toLong()
 }
 
 fun solvePart2(input: String): Long {
